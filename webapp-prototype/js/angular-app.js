4e528128db39ed8359a819a001809c3e0b6f9401
Micah Hauge <micahhauge@gmail.com>
2017-10-21 10:16:52 -0500

    Added ip table

--- webapp-prototype/js/angular-app.js
+++ webapp-prototype/js/angular-app.js
@@ -6,6 +6,15 @@ angular.module('app', ['ngRoute'])
         { type: 'Torrenting ', severity: 8, description: "BitTorrent Client Detected", ip: '124.23.2584'},
         { type: 'DDOS attack', severity: 10, description: "Someone is attempting a DDOS attack!", ip: '666.23.2584'},
     ];
+
+    $scope.connections= [
+        { ip: "1.42.152.35", device: 'MacbookPro', timeOnNetwork: '01:23:13'},
+        { ip: "8.41.102.35", device: 'Dell latitude', timeOnNetwork: '00:03:13'},
+        { ip: "7.21.12.359", device: 'IOS Device', timeOnNetwork: '00:20:02'},
+        { ip: "1.42.152.35", device: 'Thinkpad', timeOnNetwork: '01:23:13'},
+        { ip: "7.21.12.359", device: 'Android Device', timeOnNetwork: '00:20:02'},
+        { ip: "1.42.152.35", device: 'MacbookPro', timeOnNetwork: '01:23:13'},
+    ];
   }])
 
   .config(['$routeProvider', function ($routeProvider) {
